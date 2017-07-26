7a5948bc6d59dcd4eb7583b6cfea3a10cff23d9d
Mate Soos <soos.mate@gmail.com>
2017-07-26 22:03:27 +0100

    Updating to new streambuffer

--- streambuffer.h
+++ streambuffer.h
@@ -1,54 +1,64 @@
 /*******************************************************************************
-MiniSat -- Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
-OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*******************************************************************************/
+ * MiniSat -- Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+ * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
 
 #ifndef STREAMBUFFER_H
 #define STREAMBUFFER_H
 
-#define CHUNK_LIMIT 1048576
+static const unsigned chunk_limit = 148576;
 
-#ifdef USE_ZLIB
-#include <zlib.h>
-typedef size_t(*fread_op_zip)(void*, size_t, size_t, gzFile);
-#endif
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
 #include <limits>
 #include <string>
+#include <memory>
 
-//A = gzFile, FILE
-//B = fread, gz_read
-typedef size_t(*fread_op_norm)(void*, size_t, size_t, FILE*);
+#ifdef USE_ZLIB
+#include <zlib.h>
+struct GZ {
+    static inline int read(void* buf, size_t num, size_t count, gzFile f)
+    {
+        return gzread(f, buf, num*count);
+    }
+};
+#endif
 
-template<typename A, typename B, B C>
+struct FN {
+    static inline int read(void* buf, size_t num, size_t count, FILE* f)
+    {
+        return fread(buf, num, count, f);
+    }
+};
+
+template<typename A, typename B>
 class StreamBuffer
 {
     A  in;
     void assureLookahead() {
         if (pos >= size) {
             pos  = 0;
-            size = C(buf, 1, sizeof(buf), in);
+            size = B::read(buf.get(), 1, chunk_limit, in);
         }
     }
-    char    buf[CHUNK_LIMIT];
     int     pos;
     int     size;
+    std::unique_ptr<char[]> buf;
 
     void advance()
     {
@@ -60,7 +70,12 @@ class StreamBuffer
     }
 
 public:
-    StreamBuffer(A i) : in(i), pos(0), size(0) {
+    StreamBuffer(A i) :
+    in(i)
+    , pos(0)
+    , size(0)
+    , buf(new char[chunk_limit]())
+    {
         assureLookahead();
     }
 
@@ -81,13 +96,13 @@ public:
         }
     }
 
-    bool skipLine()
+    void skipLine()
     {
         for (;;) {
-            if (value() == EOF || value() == '\0') return false;
+            if (value() == EOF || value() == '\0') return;
             if (value() == '\n') {
                 advance();
-                return true;
+                return;
             }
             advance();
         }
@@ -159,7 +174,6 @@ public:
             c = value();
         }
         ret = mult*val;
-        //std::cout << "Read int " << ret << " at line " << lineNum << std::endl;
         return true;
     }
 
