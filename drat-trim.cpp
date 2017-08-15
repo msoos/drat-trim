f1264cfaae87bb26d0b1047b10a1edb3fae1b2fb
Mate Soos <soos.mate@gmail.com>
2017-08-15 23:04:53 +0100

    No need for this time.h, it's non-portable

--- drat-trim.cpp
+++ drat-trim.cpp
@@ -21,7 +21,6 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <sys/time.h>
 #include <unistd.h>
 
 
