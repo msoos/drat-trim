02dd3356e8226a0223bd2fbd15b2c1982b0bdadf
Mate Soos <soos.mate@gmail.com>
2017-08-31 22:51:53 +0100

    Use cstdio, it's easier

--- drat-trim.cpp
+++ drat-trim.cpp
@@ -18,7 +18,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include <stdio.h>
+#include <cstdio>
 #include <cstdlib>
 #include <cassert>
 #include <cstdint>
